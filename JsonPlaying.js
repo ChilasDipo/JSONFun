398b85459fd1b5cfe58a1d995bf5ec3d2ebc0b0d
ChilasDipo <Chil0041@edu.easj.dk>
2021-10-27 12:23:04 +0200

    added html

--- JsonPlaying.js
+++ JsonPlaying.js
@@ -1,23 +1,18 @@
-JSONObejct = {
-	"Folk": [{
-		"name": "Gustav",
-		"age": "old",
-		"height": "Tall"
-	}, {
-		"name": "Chilas",
-		"age": "Less Old",
-		"height": "Less Tall"
-	},{
-		"name": "TallBoy",
-		"age": "Not Old",
-		"height": "Tallest"
-	}]
+const xhttp = new XMLHttpRequest()
+function run() {
+   xhttp.open("GET", "https://gist.githubusercontent.com/ChilasDipo/aa6e87d05dbb3d283bca417c6a48bdde/raw/a77867a6dd9d957a4f467a97683fac7d7033be30/JsonPlaying")
+   xhttp.onload = function() {
+	  
+	  let JsonOBJ = JSON.parse(this.response)
+	  for (let index = 0; index < JsonOBJ.Folk.length; index++) {
+	   if(JsonOBJ.Folk[index].height == "Tallest"){
+		   document.getElementById("data").innerHTML=("The tallest is " + JsonOBJ.Folk[index].name)
+	   }
+
+   }
+	
 }
 
-for (let index = 0; index < JSONObejct.Folk.length; index++) {
-    if(JSONObejct.Folk[index].height == "Tallest"){
-        console.log("The tallest is " + JSONObejct.Folk[index].name)
-    }
-    
+xhttp.send()
 }
-
+  
